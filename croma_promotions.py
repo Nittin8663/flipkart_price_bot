003494cece427aa7d5cc3fb4c8ead983fd569227
Nittin8663 <90694841+Nittin8663@users.noreply.github.com>
2025-08-20 13:41:24 +0530

    Update croma_promotions.py

--- croma_promotions.py
+++ croma_promotions.py
@@ -1,14 +1,15 @@
-from playwright.sync_api import sync_playwright
+import requests
 
-proxy = {
-    "server": "http://gw.dataimpulse.com:10000",
-    "username": "1258bd9e03f80533eb38__cr.in",
-    "password": "ca69cf1263c65d0e",
+proxies = {
+    "http": "http://1258bd9e03f80533eb38__cr.in:ca69cf1263c65d0e@gw.dataimpulse.com:10000",
+    "https": "http://1258bd9e03f80533eb38__cr.in:ca69cf1263c65d0e@gw.dataimpulse.com:10000",
 }
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(proxy=proxy, headless=False)
-    page = browser.new_page()
-    page.goto("https://www.croma.com/")
-    print(page.title())
-    browser.close()
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/115.0.0.0 Safari/537.36"
+}
+
+url = "https://www.croma.com/"
+r = requests.get(url, headers=headers, proxies=proxies, timeout=15)
+print(r.status_code)
+print(r.text[:500])
